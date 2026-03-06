6c2d7829a1e7fd84efb2ad3563fab5abfce46643
aastngel <andrew.dn.walker00@gmail.com>
2026-03-05 17:12:04 -0800

    fix setup.py

--- src/fastapi-requests/__version__.py
+++ src/fastapi-requests/__version__.py
@@ -2,13 +2,13 @@
 # |(  |-  |.| | | |-  `-.  |  `-.
 # ' ' `-' `-`.`-' `-' `-'  '  `-'
 
-__title__ = "requests"
+__title__ = "fastapi-requests"
 __description__ = "Python HTTP for Humans."
 __url__ = "https://requests.readthedocs.io"
 __version__ = "2.32.5"
 __build__ = 0x023205
-__author__ = "Kenneth Reitz"
-__author_email__ = "me@kennethreitz.org"
+__author__ = "Andrew Walker"
+__author_email__ = "andrew.dn.walker00@gmail.com"
 __license__ = "Apache-2.0"
-__copyright__ = "Copyright Kenneth Reitz"
+__copyright__ = "Copyright Andrew Walker"
 __cake__ = "\u2728 \U0001f370 \u2728"
